5efcad4a399613ce2c9522116b064fab923289df
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-05-08 16:48:32 +0100

    create schema

---
+++ src/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Create schema
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    country: String
+});
+
+// create a collection "User" from the schema
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
