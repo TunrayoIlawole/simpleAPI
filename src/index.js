706864e64e0e9f746dcada9e6b684b9bfbb6e9d0
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2021-05-09 12:27:13 +0100

    add routes middleware

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ const app = express();
 const { PORT } = process.env;
 
 const dbSetup = require('./database/setup');
+const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json());
 
 // setup mongoose
 dbSetup();
+
+// routes
+app.use(userRoutes);
 
 // PORT
 const port = process.env.PORT || PORT;
